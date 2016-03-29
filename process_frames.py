5ec396b66ce0f1c506692b055c8e2a3ce8dde907
gSchool Student <datascientist@Datas-Mac-mini-7.local>
2016-03-29 14:11:52 -0600

    non-parallel tested and working on one file

--- process_frames.py
+++ process_frames.py
@@ -8,35 +8,51 @@ and the resolution although you could downsample after
 Bayesian update
 markov chain state change?
 '''
-
+from sys import argv
 import cv2
 import numpy as np
+import multiprocessing
+from os import listdir
+from os.path import join
+import pdb
+
+script,source,destination,POOL_SIZE, pct_frames = argv
+POOL_SIZE = int(POOL_SIZE)
+pct_frames = float(pct_frames)
+file_lst = [ join(source,f) for f in listdir(source) ] #there should only be file, not folders
+
+def get_frame_parallel(pool_size):
+    pool = multiprocessing.Pool(pool_size)
+    pool.map(get_frames,file_lst)
+
+
+def write_path(file_str,count):
+    name = file_str.split('/')[-1].split('.')[0]
+    pdb.set_trace()
+    return join(destination, name + '_' + str(count) + '.png' )
+
 
 #should you assert pcnt_frames is a float <1
-def get_frames(file_str,pct_frame):
-    vid = cv2.VideoCapture(file)
+def get_frames(file_str):
+    vid = cv2.VideoCapture(file_str)
+
     if vid.isOpened():
         frame_count = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
         step_size = int(1/float(pct_frames))
+
         count = 0
         while count <= frame_count:
-            vid.set(cv2.cv.CV_CAP_POS_FRAMES,count)
+            pdb.set_trace()
+            w_path = write_path(file_str,count)
+            vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,count)
             ret, frame = vid.read()
-            cv2.imwrite('/Users/datascientist/Desktop/images/' +  '_{}'.format(),frame)
-        vid.set(cv2.cv.CV_CAP_POS_FRAMES,)
+            cv2.imwrite(w_path,frame)
+            count+=step_size
+        #Do you need to close the VideoCapture?
     else:
         return 'could not open file'
 
 
 
-'''
-vid = cv2.VideoCapture(file_path)
-vid.isOpened()
-vid.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
-vid.set(cv2.cv.CV_CAP_POS_FRAMES)
-cv.imshow('video','frame[1]')
-
-'''
-
 if __name__ == '__main__':
-    pass
+    get_frames('data/Tide.mp4')
